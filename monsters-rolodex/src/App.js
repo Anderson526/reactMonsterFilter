be933eeb48a22d05fcb73dc40ad0b06f227def9e
AndersonC <1megaword1@gmail.com>
2023-11-01 16:03:07 -0500

    new commit
    adding a api with a fetch and a promise (then) :0

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+
+constructor(){
+  super();
+  this.state = {
+    monsters:[
+
+      {
+        name:'monsterlokis',
+        id: '12345'
+       },
+     {
+        name: 'monstermiau',
+        id: '123456'
+       },
+      {
+        name:'monsterdog',
+        id: '123457'
+       }
+
+    ]
+ 
+
+  };
+}
+
+componentDidMount(){
+  //se ejecuta cada vez que se renderice un componente 
+
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((users) => console.log(users))
+}
+
+
+render() {
+
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+{
+  this.state.monsters.map((monster) => {
+    return(
+    <div key={monster.name}>
+      <h1 >{monster.name}</h1>
+    </div>
+    ); 
+    
+  })
+}
+
     </div>
   );
 }
+}
 
 export default App;
+
+
+
